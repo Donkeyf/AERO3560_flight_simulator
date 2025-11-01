c23608dfe3e027d593650b2941dcc95267751b8b
JAnd2701 <jand2701@uni.sydney.edu.au>
2025-11-02 04:29:26 +1100

    updated documentation and ensured variables are called correctly
    moving to look at state rates now

--- AeroAngles.m
+++ AeroAngles.m
@@ -1,7 +1,15 @@
 function [alpha, beta] = AeroAngles(X)
 % Calculates the angle of attack and sideslip angles using the current
-% velocity
-    V = sqrt(X(1)^2 + X(2)^2 + X(3)^2);
-    alpha = asin(X(2)/V);
-    beta = atan(X(3)/V);
+% velocity.
+%
+% Outputs:
+% alpha - Angle of Attack (radians)
+% beta  - Sideslip Angle (radians)
+    
+    % Computing Current Velocity
+    V = norm(X(1:3));
+
+    % Calculating Aerodynamic Angles
+    alpha = atan2(X(3)/X(1));
+    beta = asin(X(2)/V);
 end
--- FlowProperties.m
+++ FlowProperties.m
@@ -1,16 +1,23 @@
-function [rho, Q] = FlowProperties(X,Flight_Data)
+function [rho, Q] = FlowProperties(Flight_Data, X)
 % Calculates the air density and dynamic pressure for the current altitude
-% and flight speed
-    R = 287;                                % J/kgK - Gas Constant of Air
+% and flight speed.
+%
+% Outputs:
+% Q     - Dynamic Pressure (Pascals)
+% rho   - Air Density (kg/m^3)
+    R = 287.05;                             % J/kgK - Gas Constant of Air
     h = -X(end);                            % m - Current Altitude
-    V = sqrt(X(1)^2 + X(2)^2 + X(3)^2);     % m/s - Current Velocity
+    V = norm(X(1:3));                       % m/s - Current Velocity
     P_sls = 101325;                         % Pa - Sea Level Pressure
     T_sls = 288.15;                         % K - Sea Level Temperature
     L = -6.5/1000;                          % K/m - Lapse Rate
-    g = Flight_Data.Inertial.g;                      % m/s^2 - Gravity Acceleration
+    g = Flight_Data.Inertial.g;             % m/s^2 - Gravity Acceleration
 
+    % Temp. & Pressure Change Due to Altitude
     T = T_sls + L*h;
     P = P_sls*(1 + (L/T_sls)*h)^(-g/(R*L));
+
+    % Flow Properties
     rho = P/(R*T);
     Q = 0.5*rho*V^2;
 end
