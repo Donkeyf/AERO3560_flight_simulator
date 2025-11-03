3c10d0779afdee0e735a61af0b81af5b366b08b8
JAnd2701 <jand2701@uni.sydney.edu.au>
2025-11-04 00:45:24 +1100

    updated main structure

--- Main.m
+++ Main.m
@@ -16,45 +16,43 @@ clear; clc; close all;
 
 % Select Manoeuvre to Perform & Generate Control Array
 [U, time] = Controls();
- 
-% % Trim aircraft
-% [X_0_trim, U_input_trim] = Trim(Flight_Data, X_0);
-% disp(X_0_trim);
-% 
-% % Add path to file folder
-% % addpath('AircraftData');
-% 
-% % Matrices to recieve state rates X_data, U_data and time_data
-% X_data = {};                % State rates for each time time step
-% U_data = {};                % Control inputs including as varied in order to trim aircraft
-% time_data = {};             % Time vector which Total time divided by dt (time step)
-% 
-% % Total time and time step
-% dt = 0.01;                  % Time step in seconds
-% total_time = 200;           % Total time in seconds for simulation
-% time = 0:dt:total_time;     % Time vector
-% time_data{1} = time;        % Time vector stored
-% 
-% % Set up solution matrices for simulation
-% X = zeros(length(X_0_trim), length(time));
-% U = zeros (length(U_input_trim), length(time));
-% X(:,1) = X_0_trim;          % Inital state vector now trimmed
-% U(:,1) = U_input_trim;      % Initial control input now trimmed
-% 
-% % Loop through simulation
-% for i = 2: length(time)
-%     U_temp = Controls(i,U_input,U_input_trim); % Check Control
-%     % Integrate at this time step to get new state vector
-%     X(:,i) = Integrate(Flight_Data, X(:,i-1), U_temp, time(i), dt);
-%     U(:,i) = U-temp;
-% end
-% 
-% % Copy results from the integration into matrices for plotting
-% X_data{1} = X;
-% U_data{1} = U;
-% 
-% % Plot the simulation results for each state rate variable against time
-% PlotData(X_data, U_data, time_data, Flight_Condition);
+
+% Extracting timestep for integration
+dt = time(2) - time(1);
+
+% Trim aircraft
+[Xtrim, Utrim] = Trim(Flight_Data, X0);
+
+% Initialising states array & adding initial state
+X = zeros(length(X0),length(time));
+X(:,1) = X0;
+
+% Initialising state to loop with
+X_i = X0;
+
+% Control Array
+U = Utrim + U;
+
+% Loop through time array and integrate at each timestep
+for i = 1:length(time)
+    % Timestep specific control vector and state
+    U_i = U(:,i);
+
+    % Calculate state rates
+    XDot = StateRates(Flight_Data,X_i,U_i);
+
+    % Integrate state rates & update state
+    X_new = Integrate(Flight_Data,X_i,U_i,dt);
+
+    % Save new state to array and update
+    X(:,i) = X_new;
+    X_i = X_new;
+end
+
+% Plot Results
+% PlotData()
+
+
 
 
 
